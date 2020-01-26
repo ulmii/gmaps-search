3d20c23f16414fd13a16295bfbe641cb8172e1e8
Konrad Ulman <veradopl@gmail.com>
2020-01-26 20:47:10 +0100

    Remove api key

--- src/main/resources/static/scripts/controllers/map.js
+++ src/main/resources/static/scripts/controllers/map.js
@@ -2,11 +2,11 @@
 
 angular.module('App')
     .controller('MapCtrl', ['$scope', '$rootScope', '$http', '$window', '$timeout', '$compile', function ($scope, $rootScope, $http, $window, $timeout, $compile) {
+        var API_KEY = '';
         var params = (new URL(document.location)).searchParams;
         var name = params.get('name');
         var capital = params.get('capital');
-        var mapsUrl = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyCaYeCmNWNFLv9XVeOYvCY4k4jLA7Gwa-Y&q=' + name + ',' + capital;
-
+        var mapsUrl = 'https://www.google.com/maps/embed/v1/place?key=' + API_KEY + '&q=' + name + ',' + capital;
         var html = '<iframe \
       width="1200" \
       height="700" \
